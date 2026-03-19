61f68005f7e833d4109c5632e74ebba652673abe
Yurii Karabas <1998uriyyo@gmail.com>
2026-03-19 19:45:09 +0100

    Update twitter consts vars

--- x_twitter_thread_dump/consts.py
+++ x_twitter_thread_dump/consts.py
@@ -5,13 +5,14 @@ DEFAULT_BEARER_TOKEN = (
     "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"  # noqa: S105
 )
 
-TWEET_RESULT_BY_REST_ID_PATH = "/graphql/Opujkru5iJSDWj4DuJISOw/TweetResultByRestId"
+TWEET_RESULT_BY_REST_ID_PATH = "/graphql/zy39CwTyYhU-_0LP7dljjg/TweetResultByRestId"
 TWEET_RESULT_BY_REST_ID_PARAMS = {
     "variables": {
         "tweetId": None,
-        "withCommunity": False,
-        "includePromotedContent": False,
-        "withVoice": False,
+        "includePromotedContent": True,
+        "withBirdwatchNotes": True,
+        "withVoice": True,
+        "withCommunity": True,
     },
     "features": {
         "creator_subscriptions_tweet_preview_api_enabled": True,
@@ -20,8 +21,9 @@ TWEET_RESULT_BY_REST_ID_PARAMS = {
         "c9s_tweet_anatomy_moderator_badge_enabled": True,
         "responsive_web_grok_analyze_button_fetch_trends_enabled": False,
         "responsive_web_grok_analyze_post_followups_enabled": False,
-        "responsive_web_jetfuel_frame": False,
+        "responsive_web_jetfuel_frame": True,
         "responsive_web_grok_share_attachment_enabled": True,
+        "responsive_web_grok_annotations_enabled": True,
         "articles_preview_enabled": True,
         "responsive_web_edit_tweet_api_enabled": True,
         "graphql_is_translatable_rweb_tweet_is_translatable_enabled": True,
@@ -29,18 +31,23 @@ TWEET_RESULT_BY_REST_ID_PARAMS = {
         "longform_notetweets_consumption_enabled": True,
         "responsive_web_twitter_article_tweet_consumption_enabled": True,
         "tweet_awards_web_tipping_enabled": False,
+        "content_disclosure_indicator_enabled": True,
+        "content_disclosure_ai_generated_indicator_enabled": True,
         "responsive_web_grok_show_grok_translated_post": False,
-        "responsive_web_grok_analysis_button_from_backend": False,
-        "creator_subscriptions_quote_tweet_preview_enabled": False,
+        "responsive_web_grok_analysis_button_from_backend": True,
+        "post_ctas_fetch_enabled": False,
         "freedom_of_speech_not_reach_fetch_enabled": True,
         "standardized_nudges_misinfo": True,
         "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": True,
         "longform_notetweets_rich_text_read_enabled": True,
-        "longform_notetweets_inline_media_enabled": True,
+        "longform_notetweets_inline_media_enabled": False,
         "profile_label_improvements_pcf_label_in_post_enabled": True,
-        "rweb_tipjar_consumption_enabled": True,
+        "responsive_web_profile_redirect_enabled": False,
+        "rweb_tipjar_consumption_enabled": False,
         "verified_phone_label_enabled": False,
         "responsive_web_grok_image_annotation_enabled": True,
+        "responsive_web_grok_imagine_annotation_enabled": True,
+        "responsive_web_grok_community_note_auto_translation_is_enabled": False,
         "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False,
         "responsive_web_graphql_timeline_navigation_enabled": True,
         "responsive_web_enhance_cards_enabled": False,
@@ -48,8 +55,8 @@ TWEET_RESULT_BY_REST_ID_PARAMS = {
     "fieldToggles": {
         "withArticleRichContentState": True,
         "withArticlePlainText": False,
-        "withGrokAnalyze": False,
-        "withDisallowedReplyControls": False,
+        "withArticleSummaryText": True,
+        "withArticleVoiceOver": True,
     },
 }
 
